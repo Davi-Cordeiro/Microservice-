3a90e94bd4b69a96cf8cf4299ae443ebbe2552b8
Davi-Cordeiro <davicordeiro01012008@gmail.com>
2026-09-02 22:23:15 -0300

    Update(ApiRest): criacao da api rest para comunicar o java com o django

--- ecommerc_django/produtos/models.py
+++ ecommerc_django/produtos/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    product_name = models.CharField(("nome do produto"), max_length=50)
+    description = models.TextField(("Descrição do produto"), max_length=2000)
+
+    def __str__(self):
+        return self.product_name
--- ecommerc_django/produtos/views.py
+++ ecommerc_django/produtos/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import Product
 
-# Create your views here.
+
+def home(request):
+    return render(request, 'ecommerc_django/produtos/templates/home.html')
+
+
+class ProductSerializer(APIView):
+    def get(self, response):
+        produtos = Product.objects.filter.all()
+        serializer = ProductSerializer(produtos)
+
+        return Response(serializer.data)
