00aef845110a0680495a86c8475dd1e9227f6320
Davi-Cordeiro <davicordeiro01012008@gmail.com>
2026-09-10 19:31:31 -0300

    Adicionando o cadastro de produto dentro da url.py

--- ecommerc_django/produtos/urls.py
+++ ecommerc_django/produtos/urls.py
@@ -1,9 +1,10 @@
 
 from django.urls import path
 from django.urls import include
-from .views import ProductView
+from .views import ProductView, ValidadeProductForm
 
 urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('api/', ProductView.as_view(), name="produtos_api")
+    path('produto/cadastro', ValidadeProductForm.as_view(), name="Cadastro de novo produto")
 ]
