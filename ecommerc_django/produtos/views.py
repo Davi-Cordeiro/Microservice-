ef2b8b3ee21adfe8fcc52ffbe3f8d8fbf570e01f
Davi-Cordeiro <davicordeiro01012008@gmail.com>
2026-09-10 16:54:05 -0300

    feature(views): criacao de get e post do forms do django

--- ecommerc_django/produtos/views.py
+++ ecommerc_django/produtos/views.py
@@ -1,7 +1,10 @@
+from django import views
+from django.http import JsonResponse, request
 from django.shortcuts import render
 from rest_framework.generics import ListAPIView
 from .serializers import ProductSerializer
 from .models import Product
+from django.forms import ModelForm
 
 
 def home(request):
@@ -11,3 +14,20 @@ def home(request):
 class ProductView(ListAPIView):
     queryset = Product.objects.filter(active=True)
     serializer_class = ProductSerializer
+
+class ProductForm(ModelForm):
+    class Meta:
+        model = Product
+        exclude = ('active',)
+
+class ValidadeProductForm(views.View):
+    def post(self, request, *args, **kwargs):
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            return JsonResponse({'valid': True})
+        else:
+            return JsonResponse({'valid': False, 'errors': form.errors})
+
+    def get(self, request, *args, **kwargs):
+        form = ProductForm()
+        return render(request, 'product_form.html', {'form': form})
