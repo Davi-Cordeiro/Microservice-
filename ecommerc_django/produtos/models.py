99e7bae3795244c91488855feea4f65d5750a7af
Davi-Cordeiro <davicordeiro01012008@gmail.com>
2026-09-15 14:57:13 -0300

    fix: correcao de conflito

--- ecommerc_django/produtos/models.py
+++ ecommerc_django/produtos/models.py
@@ -14,11 +14,7 @@ class Product(models.Model):
     product_name = models.CharField(("nome do produto"), max_length=50, unique=True)
     description = models.TextField(("Descrição do produto"), max_length=2000)
     created_at = models.DateTimeField(auto_now_add=True)
-<<<<<<< HEAD
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True, related_name="products")
-=======
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
->>>>>>> origin/main
     storage = models.PositiveIntegerField(default=0)
     active = models.BooleanField(default=True)
 
