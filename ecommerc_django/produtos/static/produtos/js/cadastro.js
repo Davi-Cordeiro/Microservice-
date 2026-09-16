eec1ae721e0fd16b004e951d03033dfdcadaf4bd
Davi-Cordeiro <davicordeiro01012008@gmail.com>
2026-09-16 15:32:59 -0300

    update(cadastro): criacao do javascript para receber o form e enviar pra api

--- ecommerc_django/produtos/static/produtos/js/cadastro.js
+++ ecommerc_django/produtos/static/produtos/js/cadastro.js
@@ -1,10 +1,40 @@
 const button = document.getElementById('submit-btn');
-    button.addEventListener('click', function(event) {
+
+    button.addEventListener('click', async function(event) {
         event.preventDefault();
         const form = document.getElementById('cadastro-form');
         const formData = new FormData(form);
 
-        for (const [key, value] of formData.entries()) {
-            console.log(`${key}: ${value}`);
+
+        try{
+            const response = await fetch(form.action, {
+                method: form.method,
+                body: formData,
+            });
+
+            if (response.ok) {
+                const data = await response.json();
+                console.log(data);
+                
+                if (data.valid) {
+                    console.log('Produto cadastrado com sucesso!');
+                    alert('Produto cadastrado com sucesso!');
+                    form.reset();
+
+                } else {
+                    console.log('Erro ao cadastrar produto: '+ data.errors);
+                    alert('Erro ao cadastrar produto: ' + data.errors);
+                }
+
+            }
+
+            else {
+                console.log(form.action);
+                throw new Error('Erro ao cadastrar produto: ' + response.status);
+                
+            }
+
+        } catch (error) {
+            alert('Erro ao cadastrar produto: ' + error.message);
         }
-    });
+});
